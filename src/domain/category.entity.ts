edcef918838f0e29873256dbf04b12400e02f9eb
Breno Battaglin <breno.battaglin@icloud.com>
2024-04-01 16:54:47 -0300

    chore: create category entity

---
+++ src/domain/category.entity.ts
@@ -0,0 +1,37 @@
+export type CategoryConstructorProps = {
+  categoryId?: string;
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+  createdAt?: Date;
+};
+
+export type CategoryCreateCommand = {
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+};
+
+export class Category {
+  categoryId: string;
+  name: string;
+  description: string | null;
+  isActive: boolean;
+  createdAt: Date;
+
+  constructor(props: CategoryConstructorProps) {
+    this.categoryId = props.categoryId;
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.isActive = props.isActive ?? true;
+    this.createdAt = props.createdAt ?? new Date();
+  }
+
+  static create(props: CategoryConstructorProps): Category {
+    return new Category(props);
+  }
+
+  update(props: Partial<CategoryConstructorProps>): Category {
+    return new Category({ ...this, ...props });
+  }
+}
