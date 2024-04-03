61a0136cc20cbbdf84c018b5504196d132677086
Breno Battaglin <breno.battaglin@icloud.com>
2024-04-02 22:22:11 -0300

    test: create category unit tests
    - create 'changes' tests
    - create 'create' factory tests

--- src/domain/category.entity.spec.ts
+++ src/domain/category.entity.spec.ts
@@ -13,7 +13,7 @@ describe("category unit tests", () => {
       expect(category.createdAt).toBeInstanceOf(Date);
     });
 
-    it("should return new category without description", () => {
+    it("should return new category with description", () => {
       const createdAt = new Date();
       const category = new Category({
         name: "Movie",
@@ -26,6 +26,69 @@ describe("category unit tests", () => {
       expect(category.description).toBe("Movie desc");
       expect(category.isActive).toBe(false);
       expect(category.createdAt).toBe(createdAt);
+    });
+  });
+  describe("when category is created", () => {
+    test("should create a category without", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      expect(category.categoryId).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.isActive).toBe(true);
+      expect(category.createdAt).toBeInstanceOf(Date);
+    });
+
+    it("should create category with description", () => {
+      const createdAt = new Date();
+      const category = Category.create({
+        name: "Movie",
+        description: "Movie desc",
+        isActive: false,
+      });
+      expect(category.categoryId).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("Movie desc");
+      expect(category.isActive).toBe(false);
+      expect(category.createdAt).toBeInstanceOf(Date);
+    });
+  });
+
+  describe("when changes are applied", () => {
+    it("should change name", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      category.changeName("Music");
+      expect(category.name).toBe("Music");
+    });
+
+    it("should change description", () => {
+      const category = Category.create({
+        name: "Movie",
+        description: "Movie desc",
+      });
+      category.changeDescription("Music desc");
+      expect(category.description).toBe("Music desc");
+    });
+
+    it("should activate category", () => {
+      const category = Category.create({
+        name: "Movie",
+        isActive: false,
+      });
+      category.activate();
+      expect(category.isActive).toBe(true);
+    });
+
+    it("should deactivate category", () => {
+      const category = Category.create({
+        name: "Movie",
+        isActive: true,
+      });
+      category.deactivate();
+      expect(category.isActive).toBe(false);
     });
   });
 });
