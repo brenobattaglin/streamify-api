c91900675c59aa190c464ca04a7a372d089ffeb2
Breno Battaglin <breno.battaglin@icloud.com>
2024-04-06 18:50:26 -0300

    chore: create category validator

---
+++ src/shared/domain/validators/validator-fields-interface.ts
@@ -0,0 +1,10 @@
+export type FieldErrors = {
+    [field: string]: string[]
+};
+
+export interface IValidatorFields<PropsValidated> {
+    errors: FieldErrors | null;
+    validatedData: PropsValidated | null;
+
+    validate(data: any): boolean;
+}
