8c90dc825b739a25c0dcbc8cb9aee792dbf0edb1
Breno Battaglin <breno.battaglin@icloud.com>
2024-05-03 21:16:25 -0300

    test: wip create in memory repository tests

--- src/shared/domain/entity.ts
+++ src/shared/domain/entity.ts
@@ -1,6 +1,6 @@
-import { ValueObject } from "./value-object";
+import { ValueObject } from './value-object';
 
 export abstract class Entity {
-    abstract get entityId(): ValueObject;
-    abstract toJSON(): any;
+  abstract get entityId(): ValueObject;
+  abstract toJSON(): any;
 }
